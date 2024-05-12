d73c9ddec5f74bc741f5d21671abb132c33168e6
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-05-12 15:40:17 +0900

    feat: set up repository for algorithm

---
+++ src/main/java/com/algorithm/algorithmpratice/AlgorithmPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.algorithmpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPraticeApplication.class, args);
+    }
+
+}
