68e38f2601c23c5eaba6984e1bd7023110db937f
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-08-19 09:30:43 +0900

    don't solve q15686

---
+++ src/main/java/com/algorithm/algorithmpratice/a240816_q15686_치킨배달_g5.java
@@ -0,0 +1,38 @@
+//package com.algorithm.algorithmpratice;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class a240816_q15686_치킨배달_g5 {
+//
+//    static BufferedReader br;
+//    static int n;
+//    static int m;   //폐업시키지 않을 치킨집을 최대 M개
+//
+//    static int map[][];
+//
+//    static int infoMap[];  //각
+//
+//    public static void main(String args[]) throws IOException {
+//        br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        n = Integer.parseInt(st.nextToken());
+//        m = Integer.parseInt(st.nextToken());
+//
+//        map = new int[n][n];
+//
+//        for (int i = 0; i < n; i++) {
+//            StringTokenizer st2 = new StringTokenizer(br.readLine());
+//            for (int j = 0; j < n; j++) {
+//                map[i][j] = Integer.parseInt(st2.nextToken());
+//            }
+//        }
+//
+//        //0은 빈 칸, 1은 집, 2는 치킨집
+//
+//        //각 집에서
+//    }
+//}
